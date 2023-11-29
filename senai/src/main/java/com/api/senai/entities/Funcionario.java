ac176defccdb73daae0c9b1168ac9956094b5583
digocoutinho78 <digocoutinho@outlook.com>
2023-11-29 16:08:11 -0300

    aplicacao lombok cliente func e pessoa

--- senai/src/main/java/com/api/senai/entities/Funcionario.java
+++ senai/src/main/java/com/api/senai/entities/Funcionario.java
@@ -4,54 +4,38 @@ import java.sql.Timestamp;
 
 import com.api.senai.entities.Pessoa;
 
+import jakarta.annotation.Generated;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+
+@Data
+@Entity
+@EqualsAndHashCode(callSuper = false)
+@Table(name = "funcionario")
+
+
 public class Funcionario extends Pessoa{
     
-    private String funcId ;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long funcId ;
     private Agencia agencia;
     private String cargo;
     private Timestamp dataExclusao;
     private Boolean ativo;
+    private String senha;
 
-    public Funcionario(String nome, String cpf, Endereco endereco, Timestamp dataNasc, String telefone, String email,
-            Timestamp dataCad, String funcId, Agencia agencia, String cargo, Timestamp dataExclusao, Boolean ativo) {
-        super(nome, cpf, endereco, dataNasc, telefone, email, dataCad);
-        this.funcId = funcId;
-        this.agencia = agencia;
-        this.cargo = cargo;
-        this.dataExclusao = dataExclusao;
-        this.ativo = ativo;
-    }
-    
-    public String getFuncId() {
-        return funcId;
-    }
-    public void setFuncId(String funcId) {
-        this.funcId = funcId;
-    }
-    public Agencia getAgencia() {
-        return agencia;
-    }
-    public void setAgencia(Agencia agencia) {
-        this.agencia = agencia;
-    }
-    public String getCargo() {
-        return cargo;
-    }
-    public void setCargo(String cargo) {
-        this.cargo = cargo;
-    }
-    public Timestamp getDataExclusao() {
-        return dataExclusao;
-    }
-    public void setDataExclusao(Timestamp dataExclusao) {
-        this.dataExclusao = dataExclusao;
-    }
-    public Boolean getAtivo() {
-        return ativo;
-    }
-    public void setAtivo(Boolean ativo) {
-        this.ativo = ativo;
+
+
     }
 
+
     
-}
+
