5579865d3c8ec0020a4ed0ff2dc67fb198e1145e
digocoutinho78 <digocoutinho@outlook.com>
2023-11-29 15:48:08 -0300

    atualizacao pessoa mapeamento

--- senai/src/main/java/com/api/senai/entities/Pessoa.java
+++ senai/src/main/java/com/api/senai/entities/Pessoa.java
@@ -2,72 +2,32 @@ package com.api.senai.entities;
 
 import java.sql.Timestamp;
 
-public class Pessoa {
-    
-private String nome; 
-private String cpf;
-private Endereco endereco;
-private Timestamp dataNasc;
-private String telefone;
-private String email;
-private Timestamp dataCad;
+import jakarta.persistence.ForeignKey;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.OneToOne;
+import lombok.Data;
 
 
 
-public Pessoa(String nome, String cpf, Endereco endereco, Timestamp dataNasc, String telefone, String email,
-        Timestamp dataCad) {
-    this.nome = nome;
-    this.cpf = cpf;
-    this.endereco = endereco;
-    this.dataNasc = dataNasc;
-    this.telefone = telefone;
-    this.email = email;
-    this.dataCad = dataCad;
-}
 
-public String getNome() {
-    return nome;
-}
-public void setNome(String nome) {
-    this.nome = nome;
-}
-public String getCpf() {
-    return cpf;
-}
-public void setCpf(String cpf) {
-    this.cpf = cpf;
-}
-public Endereco getEndereco() {
-    return endereco;
-}
-public void setEndereco(Endereco endereco) {
-    this.endereco = endereco;
-}
-public Timestamp getDataNasc() {
-    return dataNasc;
-}
-public void setDataNasc(Timestamp dataNasc) {
-    this.dataNasc = dataNasc;
-}
-public String getTelefone() {
-    return telefone;
-}
-public void setTelefone(String telefone) {
-    this.telefone = telefone;
-}
-public String getEmail() {
-    return email;
-}
-public void setEmail(String email) {
-    this.email = email;
-}
-public Timestamp getDataCad() {
-    return dataCad;
-}
-public void setDataCad(Timestamp dataCad) {
-    this.dataCad = dataCad;
-}
+@Data
+@MappedSuperclass
+public abstract class Pessoa {
+    private String nome; 
+    private String cpf;
+    private Endereco endereco;
+    private Timestamp dataNasc;
+    private String telefone;
+    private String email;
+    private Timestamp dataCad; // data cadastro
 
+@OneToOne
+
+// Abaixo para alinhar com o ViaCep
+
+// @JoinColumn (name = "endereco", foreignKey = @ForeignKey(name = "endereco_fkey"))
+// private Endereco endereco;
 
  
 }
