990f01dd981b06be6d026461fa6ae2bd3684eb40
digocoutinho78 <digocoutinho@outlook.com>
2023-11-29 15:06:34 -0300

    instalacao lombok e jpa

--- senai/src/main/java/com/api/senai/entities/Cliente.java
+++ senai/src/main/java/com/api/senai/entities/Cliente.java
@@ -2,7 +2,27 @@ package com.api.senai.entities;
 
 import java.sql.Timestamp;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.convert.DataSizeUnit;
+
+
+
 import com.api.senai.entities.Pessoa;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+
+
+
+@Data
+@Entity
+@EqualsAndHashCode(callSuper = false)
+@Table(name = "cliente")
 
 public class Cliente extends Pessoa{
     
@@ -10,6 +30,8 @@ public class Cliente extends Pessoa{
     private String clienteId;
     private Timestamp dataExclusao;
     private  Boolean ativo;
+    
+
     
     public Cliente(String nome, String cpf, Endereco endereco, Timestamp dataNasc, String telefone, String email,
             Timestamp dataCad, Conta conta, String clienteId, Timestamp dataExclusao, Boolean ativo) {
@@ -20,30 +42,5 @@ public class Cliente extends Pessoa{
         this.ativo = ativo;
     }
     
-    public Conta getConta() {
-        return conta;
-    }
-    public void setConta(Conta conta) {
-        this.conta = conta;
-    }
-    public String getClienteId() {
-        return clienteId;
-    }
-    public void setClienteId(String clienteId) {
-        this.clienteId = clienteId;
-    }
-    public Timestamp getDataExclusao() {
-        return dataExclusao;
-    }
-    public void setDataExclusao(Timestamp dataExclusao) {
-        this.dataExclusao = dataExclusao;
-    }
-    public Boolean getAtivo() {
-        return ativo;
-    }
-    public void setAtivo(Boolean ativo) {
-        this.ativo = ativo;
-    }
-
     
 }
